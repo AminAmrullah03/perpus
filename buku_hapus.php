f162cafdcc02c20ab7fd7c03923a279920e09cb4
AminAmrullah03 <vickyblendes25@gmail.com>
2024-03-16 13:19:16 +0700

    hapus buku fix

--- buku_hapus.php
+++ buku_hapus.php
@@ -1,6 +1,11 @@
 <?php
 $id = $_GET['id'];
+
+mysqli_query($koneksi, 'SET FOREIGN_KEY_CHECKS=0');
+
 $query = mysqli_query($koneksi, "DELETE FROM buku WHERE id_buku=$id");
+
+mysqli_query($koneksi, 'SET FOREIGN_KEY_CHECKS=1');
 
 ?>
 <script>
